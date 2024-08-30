ec098772f042ffd71ec3eb8481a6f6fe069105e8
kevin cuadros <kevindavidcuadros@gmail.com>
2024-08-30 17:31:03 -0500

    add rectangle when touch screen add physics to reactangle

--- Project-11-pachinko-game/GameScene.swift
+++ Project-11-pachinko-game/GameScene.swift
@@ -3,9 +3,24 @@ import SpriteKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-    }
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 422, y: 195)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
+        // Border limits
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
+            box.position = location
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+            addChild(box)
+        }
+    }
+    
 }
